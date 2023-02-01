3ad27c40f087068ee11a192a3142ef0bf052d215
Łukasz Wala <84684231+LVala@users.noreply.github.com>
2023-02-01 20:18:53 +0100

    [RTC-78] Setup CI (#6)
    * Add circleCI template
    
    * Add test coverage configuration
    
    * Add circleCI config
    
    * Fixes in circleCI config
    
    * Remove example test
    
    * Add status badges to README.md
    
    * Fix credo and dialyzer warnings
    
    * Fix formatting issues
    
    * Remove unnecessary typespecs and modify credo config
    
    * Apply requested changes
    
    * Get rid of unused deps in mix.lock
    
    * Apply requested changes

--- lib/jellyfish_web/telemetry.ex
+++ lib/jellyfish_web/telemetry.ex
@@ -1,4 +1,5 @@
 defmodule JellyfishWeb.Telemetry do
+  @moduledoc false
   use Supervisor
   import Telemetry.Metrics
 
--- mix.exs
+++ mix.exs
@@ -1,9 +1,6 @@
 defmodule Jellyfish.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
-  @github_url "https://github.com/jellyfish-dev/jellyfish"
-
   def project do
     [
       app: :jellyfish,
@@ -16,16 +13,19 @@ defmodule Jellyfish.MixProject do
       deps: deps(),
       dialyzer: dialyzer(),
 
+      # hex
+      description: "Jellyfish media server",
+      package: package(),
 
-            # hex
-            description: "Template Plugin for Membrane Multimedia Framework",
-            package: package(),
-
-            # docs
-            name: "Membrane Template plugin",
-            source_url: @github_url,
-            homepage_url: "https://membraneframework.org",
-            docs: docs()
+      # test coverage
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test
+      ]
     ]
   end
 
@@ -55,10 +55,12 @@ defmodule Jellyfish.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
 
-      # Docs and credo
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      # Dialyzer and credo
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
-      {:credo, ">= 0.0.0", only: :dev, runtime: false}
+      {:credo, ">= 0.0.0", only: :dev, runtime: false},
+
+      # Test deps
+      {:excoveralls, "~> 0.15.0", only: :test, runtime: false}
     ]
   end
 
@@ -74,7 +76,6 @@ defmodule Jellyfish.MixProject do
     ]
   end
 
-
   defp dialyzer() do
     opts = [
       flags: [:error_handling]
@@ -88,25 +89,14 @@ defmodule Jellyfish.MixProject do
     end
   end
 
-
   defp package do
     [
       maintainers: ["Membrane Team"],
       licenses: ["Apache-2.0"],
       links: %{
-        "GitHub" => @github_url,
-        "Membrane Framework Homepage" => "https://membraneframework.org"
+        "GitHub" => "https://github.com/jellyfish-dev/jellyfish",
+        "Membrane Framework Homepage" => "https://membrane.stream"
       }
-    ]
-  end
-
-  defp docs do
-    [
-      main: "readme",
-      extras: ["README.md", "LICENSE"],
-      formatters: ["html"],
-      source_ref: "v#{@version}",
-      nest_modules_by_prefix: [Membrane.Template]
     ]
   end
 end
