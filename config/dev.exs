74cc1c7b59efa9b48ed29d5147070f971f63be2f
Michał Śledź <michalsledz34@gmail.com>
2023-04-13 20:11:48 +0200

    [RTC-208] Add basic server notifications (#27)

--- config/dev.exs
+++ config/dev.exs
@@ -40,7 +40,7 @@ config :jellyfish, JellyfishWeb.Endpoint,
 # different ports.
 
 # Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
+config :logger, :console, level: :info, format: "[$level] $message\n"
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
---
+++ lib/jellyfish_web/server_socket .ex
@@ -0,0 +1,123 @@
+defmodule JellyfishWeb.ServerSocket do
+  @moduledoc false
+  @behaviour Phoenix.Socket.Transport
+  require Logger
+
+  @heartbeat_interval 30_000
+
+  @impl true
+  def child_spec(_opts) do
+    # No additional processes are spawned, returning child_spec for dummy task
+    %{id: __MODULE__, start: {Task, :start_link, [fn -> :ok end]}, restart: :transient}
+  end
+
+  @impl true
+  def connect(state) do
+    Logger.info("New incoming server WebSocket connection, accepting")
+
+    {:ok, state}
+  end
+
+  @impl true
+  def init(state) do
+    {:ok, Map.put(state, :authenticated?, false)}
+  end
+
+  @impl true
+  def handle_in({encoded_message, [opcode: :text]}, %{authenticated?: false} = state) do
+    case Jason.decode(encoded_message) do
+      {:ok, %{"type" => "controlMessage", "data" => %{"type" => "authRequest", "token" => token}}} ->
+        if token == Application.fetch_env!(:jellyfish, :token) do
+          :ok = Phoenix.PubSub.subscribe(Jellyfish.PubSub, "server")
+          Process.send_after(self(), :send_ping, @heartbeat_interval)
+
+          message =
+            %{"type" => "authenticated"}
+            |> control_message()
+
+          state = %{state | authenticated?: true}
+
+          Logger.info("Server WS authenticated.")
+
+          {:reply, :ok, {:text, message}, state}
+        else
+          Logger.warn("""
+          Authentication failed, reason: invalid token.
+          Closing the connection.
+          """)
+
+          # TODO 
+          # this is not in the official Phoenix documentation
+          # but in the websock_adapter 
+          # https://github.com/phoenixframework/websock_adapter/blob/main/lib/websock_adapter/cowboy_adapter.ex#L74
+          {:stop, :closed, {1000, "invalid token"}, state}
+        end
+
+      _other ->
+        Logger.warn("""
+        Received message on server WS that is not authRequest.
+        Closing the connection.
+
+        Message: #{inspect(encoded_message)}
+        """)
+
+        {:stop, :closed, 1000, state}
+    end
+  end
+
+  def handle_in({encoded_message, [opcode: :text]}, state) do
+    Logger.warn("""
+    Received message on server WS. 
+    Server WS doesn't expect to receive any messages.
+    Closing the connection.
+
+    Message: #{inspect(encoded_message)}
+    """)
+
+    {:stop, :closed, 1000, state}
+  end
+
+  @impl true
+  def handle_info(:send_ping, state) do
+    Process.send_after(self(), :send_ping, @heartbeat_interval)
+    {:push, {:ping, ""}, state}
+  end
+
+  @impl true
+  def handle_info(msg, state) do
+    msg =
+      case msg do
+        {:peer_connected, peer_id} ->
+          control_message(%{type: "peerConnected", id: peer_id})
+
+        {:peer_disconnected, peer_id} ->
+          control_message(%{type: "peerDisonnected", id: peer_id})
+
+        {:room_crashed, room_id} ->
+          control_message(%{type: "roomCrashed", id: room_id})
+
+        {:peer_crashed, peer_id} ->
+          control_message(%{type: "peerCrashed", id: peer_id})
+
+        {:component_crashed, component_id} ->
+          control_message(%{type: "componentCrashed", id: component_id})
+      end
+
+    {:push, {:text, msg}, state}
+  end
+
+  @impl true
+  def terminate(reason, _state) do
+    Logger.info("Server WebSocket stopped #{inspect(reason)}")
+
+    :ok
+  end
+
+  defp control_message(data) do
+    %{
+      "type" => "controlMessage",
+      "data" => data
+    }
+    |> Jason.encode!()
+  end
+end
