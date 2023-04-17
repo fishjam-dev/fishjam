cdde41412e1e5f3a01a708b7dc1c8bc4704ce57e
Jakub Pisarek <99591440+sgfn@users.noreply.github.com>
2023-04-17 11:30:16 +0200

    remove empty tests

--- test/jellyfish/component/rtsp_test.exs
+++ test/jellyfish/component/rtsp_test.exs
@@ -10,61 +10,48 @@ defmodule Jellyfish.Component.RTSPTest do
   @source_uri "rtsp://ef36c6dff23ecc5bbe311cc880d95dc8.se:2137/does/not/matter"
   @jellyfish_opts %{engine_pid: @engine_pid, room_id: "example-room-id"}
 
-  describe "config parser" do
-    test "sourceUri, default opts" do
-      options = Map.put(@jellyfish_opts, "sourceUri", @source_uri)
+  test "sourceUri, default opts" do
+    options = Map.put(@jellyfish_opts, "sourceUri", @source_uri)
 
-      expected = %Endpoint.RTSP{rtc_engine: @engine_pid, source_uri: @source_uri}
+    expected = %Endpoint.RTSP{rtc_engine: @engine_pid, source_uri: @source_uri}
 
-      {:ok, ^expected} = Component.RTSP.config(options)
-    end
-
-    test "sourceUri, custom opts" do
-      custom_opts = %{
-        "sourceUri" => @source_uri,
-        "rtpPort" => 34_567,
-        "maxReconnectAttempts" => 10,
-        "reconnectDelay" => 500,
-        "keepAliveInterval" => 20_000,
-        "pierceNat" => false
-      }
-
-      options = Map.merge(@jellyfish_opts, custom_opts)
-
-      expected =
-        Map.new(custom_opts, fn {k, v} -> {Macro.underscore(k) |> String.to_atom(), v} end)
-        |> Map.put(:rtc_engine, @engine_pid)
-        |> then(&struct(Endpoint.RTSP, &1))
-
-      {:ok, ^expected} = Component.RTSP.config(options)
-    end
-
-    test "error on no sourceUri" do
-      expected_reason = [
-        %OpenApiSpex.Cast.Error{
-          reason: :missing_field,
-          value: %{},
-          format: nil,
-          type: nil,
-          name: :sourceUri,
-          path: [:sourceUri],
-          length: 0,
-          meta: %{}
-        }
-      ]
-
-      {:error, ^expected_reason} = Component.RTSP.config(@jellyfish_opts)
-    end
+    {:ok, ^expected} = Component.RTSP.config(options)
   end
 
-  describe "component" do
-    test "sourceUri, default opts" do
-    end
+  test "sourceUri, custom opts" do
+    custom_opts = %{
+      "sourceUri" => @source_uri,
+      "rtpPort" => 34_567,
+      "maxReconnectAttempts" => 10,
+      "reconnectDelay" => 500,
+      "keepAliveInterval" => 20_000,
+      "pierceNat" => false
+    }
 
-    test "sourceUri, custom opts" do
-    end
+    options = Map.merge(@jellyfish_opts, custom_opts)
 
-    test "400 on no sourceUri" do
-    end
+    expected =
+      Map.new(custom_opts, fn {k, v} -> {Macro.underscore(k) |> String.to_atom(), v} end)
+      |> Map.put(:rtc_engine, @engine_pid)
+      |> then(&struct(Endpoint.RTSP, &1))
+
+    {:ok, ^expected} = Component.RTSP.config(options)
+  end
+
+  test "error on no sourceUri" do
+    expected_reason = [
+      %OpenApiSpex.Cast.Error{
+        reason: :missing_field,
+        value: %{},
+        format: nil,
+        type: nil,
+        name: :sourceUri,
+        path: [:sourceUri],
+        length: 0,
+        meta: %{}
+      }
+    ]
+
+    {:error, ^expected_reason} = Component.RTSP.config(@jellyfish_opts)
   end
 end
