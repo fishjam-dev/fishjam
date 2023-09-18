72294869c41be48b03c23378e8435a65f4db0970
Michał Śledź <michalsledz34@gmail.com>
2023-09-18 11:30:15 +0200

    Make metrics and endpoint ip and port configurable

--- config/dev.exs
+++ config/dev.exs
@@ -4,14 +4,13 @@ config :jellyfish, server_api_token: "development", dev_routes: true
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
+# We set ip and port in the runtime.exs to allow 
+# for spawning more thane one Jellyfish instance.
 #
 # The watchers configuration can be used to run external
 # watchers to your application. For example, we use it
 # with esbuild to bundle .js and .css sources.
 config :jellyfish, JellyfishWeb.Endpoint,
-  # Binding to loopback ipv4 address prevents access from other machines.
-  # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 5002],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
