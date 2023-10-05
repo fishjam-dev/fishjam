5b38ff799acee34f58009132f87f4d51c9492602
Michał Śledź <michalsledz34@gmail.com>
2023-10-05 13:03:16 +0200

    Release v0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Jellyfish.MixProject do
   def project do
     [
       app: :jellyfish,
-      version: "0.2.0-dev",
+      version: "0.2.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
