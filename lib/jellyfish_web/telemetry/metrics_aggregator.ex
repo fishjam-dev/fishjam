26342964c125adca18580318a5a3bd7960afa2fc
Jakub Pisarek <99591440+sgfn@users.noreply.github.com>
2023-10-20 14:35:29 +0200

    Add HTTP requests metrics (#106)

--- lib/jellyfish_web/telemetry/metrics_aggregator.ex
+++ lib/jellyfish_web/telemetry/metrics_aggregator.ex
@@ -8,6 +8,8 @@ defmodule JellyfishWeb.Telemetry.MetricsAggregator do
 
   @ice_received_event [Membrane.ICE, :ice, :payload, :received]
   @ice_sent_event [Membrane.ICE, :ice, :payload, :sent]
+  @http_request_event [:jellyfish_web, :request]
+  @http_response_event [:jellyfish_web, :response]
 
   @spec start_link(Keyword.t()) :: GenServer.on_start()
   def start_link(opts),
@@ -22,12 +24,22 @@ defmodule JellyfishWeb.Telemetry.MetricsAggregator do
   def handle_event(name, measurements, metadata, config)
 
   def handle_event(@ice_received_event, %{bytes: bytes}, _metadata, %{ets_table: ets_table}) do
-    :ets.update_counter(ets_table, :ingress_delta, bytes, {:ingress_delta, 0})
+    :ets.update_counter(ets_table, :ingress_delta_webrtc, bytes, {:ingress_delta_webrtc, 0})
     :ok
   end
 
   def handle_event(@ice_sent_event, %{bytes: bytes}, _metadata, %{ets_table: ets_table}) do
-    :ets.update_counter(ets_table, :egress_delta, bytes, {:egress_delta, 0})
+    :ets.update_counter(ets_table, :egress_delta_webrtc, bytes, {:egress_delta_webrtc, 0})
+    :ok
+  end
+
+  def handle_event(@http_request_event, %{bytes: bytes}, _metadata, %{ets_table: ets_table}) do
+    :ets.update_counter(ets_table, :ingress_delta_http, bytes, {:ingress_delta_http, 0})
+    :ok
+  end
+
+  def handle_event(@http_response_event, %{bytes: bytes}, _metadata, %{ets_table: ets_table}) do
+    :ets.update_counter(ets_table, :egress_delta_http, bytes, {:egress_delta_http, 0})
     :ok
   end
 
@@ -38,8 +50,14 @@ defmodule JellyfishWeb.Telemetry.MetricsAggregator do
     import Telemetry.Metrics
 
     [
-      # FIXME: The traffic metrics work only with ICE events (emitted only by WebRTC components)
-      # which means they don't count the traffic from/to RTSP and HLS components
+      # FIXME: At the moment, the traffic metrics track:
+      #   - Most HTTP traffic (Jellyfish API, HLS)
+      #   - ICE events (WebRTC)
+      #
+      # which means they don't count:
+      #   - WebSocket traffic
+      #   - RTP events (RTSP components don't use ICE)
+      #   - HTTP traffic related to metrics (not handled by Phoenix)
       sum("jellyfish.traffic.ingress.total.bytes",
         event_name: [:jellyfish],
         measurement: :traffic_ingress_total,
@@ -59,6 +77,46 @@ defmodule JellyfishWeb.Telemetry.MetricsAggregator do
         event_name: [:jellyfish],
         measurement: :traffic_egress_throughput,
         description: "Current throughput for sent traffic (bytes/second)"
+      ),
+      sum("jellyfish.traffic.ingress.webrtc.total.bytes",
+        event_name: [:jellyfish],
+        measurement: :traffic_ingress_total_webrtc,
+        description: "Total WebRTC traffic received (bytes)"
+      ),
+      last_value("jellyfish.traffic.ingress.webrtc.throughput.bytes_per_second",
+        event_name: [:jellyfish],
+        measurement: :traffic_ingress_throughput_webrtc,
+        description: "Current throughput for received WebRTC traffic (bytes/second)"
+      ),
+      sum("jellyfish.traffic.egress.webrtc.total.bytes",
+        event_name: [:jellyfish],
+        measurement: :traffic_egress_total_webrtc,
+        description: "Total WebRTC traffic sent (bytes)"
+      ),
+      last_value("jellyfish.traffic.egress.webrtc.throughput.bytes_per_second",
+        event_name: [:jellyfish],
+        measurement: :traffic_egress_throughput_webrtc,
+        description: "Current throughput for sent WebRTC traffic (bytes/second)"
+      ),
+      sum("jellyfish.traffic.ingress.http.total.bytes",
+        event_name: [:jellyfish],
+        measurement: :traffic_ingress_total_http,
+        description: "Total HTTP traffic received (bytes)"
+      ),
+      last_value("jellyfish.traffic.ingress.http.throughput.bytes_per_second",
+        event_name: [:jellyfish],
+        measurement: :traffic_ingress_throughput_http,
+        description: "Current throughput for received HTTP traffic (bytes/second)"
+      ),
+      sum("jellyfish.traffic.egress.http.total.bytes",
+        event_name: [:jellyfish],
+        measurement: :traffic_egress_total_http,
+        description: "Total HTTP traffic sent (bytes)"
+      ),
+      last_value("jellyfish.traffic.egress.http.throughput.bytes_per_second",
+        event_name: [:jellyfish],
+        measurement: :traffic_egress_throughput_http,
+        description: "Current throughput for sent HTTP traffic (bytes/second)"
       ),
       last_value("jellyfish.rooms",
         description: "Amount of rooms currently present in Jellyfish"
@@ -85,7 +143,7 @@ defmodule JellyfishWeb.Telemetry.MetricsAggregator do
 
     :telemetry.attach_many(
       __MODULE__,
-      [@ice_received_event, @ice_sent_event],
+      [@ice_received_event, @ice_sent_event, @http_request_event, @http_response_event],
       &__MODULE__.handle_event/4,
       %{ets_table: ets_table}
     )
@@ -99,18 +157,34 @@ defmodule JellyfishWeb.Telemetry.MetricsAggregator do
   def handle_info(:forward_metrics, %{ets_table: ets_table} = state) do
     rooms = Jellyfish.RoomService.list_rooms()
 
-    [ingress_delta, egress_delta] =
+    [ingress_delta_webrtc, egress_delta_webrtc, ingress_delta_http, egress_delta_http] =
       :ets.tab2list(ets_table)
       |> Enum.flat_map(fn {key, _val} -> :ets.take(ets_table, key) end)
-      |> then(fn kwl -> Enum.map([:ingress_delta, :egress_delta], &Keyword.get(kwl, &1, 0)) end)
+      |> then(fn kwl ->
+        Enum.map(
+          [:ingress_delta_webrtc, :egress_delta_webrtc, :ingress_delta_http, :egress_delta_http],
+          &Keyword.get(kwl, &1, 0)
+        )
+      end)
+
+    ingress_delta = ingress_delta_webrtc + ingress_delta_http
+    egress_delta = egress_delta_webrtc + egress_delta_http
 
     :telemetry.execute(
       [:jellyfish],
       %{
         traffic_ingress_total: ingress_delta,
-        traffic_ingress_throughput: div(ingress_delta, @metric_forwarding_interval),
+        traffic_ingress_total_webrtc: ingress_delta_webrtc,
+        traffic_ingress_total_http: ingress_delta_http,
+        traffic_ingress_throughput: rate(ingress_delta),
+        traffic_ingress_throughput_webrtc: rate(ingress_delta_webrtc),
+        traffic_ingress_throughput_http: rate(ingress_delta_http),
         traffic_egress_total: egress_delta,
-        traffic_egress_throughput: div(egress_delta, @metric_forwarding_interval),
+        traffic_egress_total_webrtc: egress_delta_webrtc,
+        traffic_egress_total_http: egress_delta_http,
+        traffic_egress_throughput: rate(egress_delta),
+        traffic_egress_throughput_webrtc: rate(egress_delta_webrtc),
+        traffic_egress_throughput_http: rate(egress_delta_http),
         rooms: Enum.count(rooms)
       }
     )
@@ -132,4 +206,6 @@ defmodule JellyfishWeb.Telemetry.MetricsAggregator do
 
     {:noreply, state}
   end
+
+  defp rate(amount), do: div(amount, @metric_forwarding_interval)
 end
