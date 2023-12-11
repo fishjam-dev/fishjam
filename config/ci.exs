d729c2fa1dbfaeb95e593a5809236c62096ca88f
Michał Śledź <michalsledz34@gmail.com>
2023-12-11 18:34:24 +0100

    Add JF_SSL_KEY_PATH and JF_SSL_CERT_PATH env vars

--- config/ci.exs
+++ config/ci.exs
@@ -1,12 +1,10 @@
 import Config
 
-config :jellyfish, server_api_token: "development"
+config :jellyfish, ip: {127, 0, 0, 1}, port: 4002, server_api_token: "development"
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :jellyfish, JellyfishWeb.Endpoint,
-  http: [ip: {127, 0, 0, 1}, port: 4002],
-  server: false
+config :jellyfish, JellyfishWeb.Endpoint, server: false
 
 # Print only warnings and errors during test
 config :logger, level: :warning
