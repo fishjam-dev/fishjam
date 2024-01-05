837c21dc67dbaa63e1c463d971d2820ec3deca39
Karol Konkol <56369082+Karolk99@users.noreply.github.com>
2024-01-05 15:09:28 +0100

    Fix sending metrics (#138)
    * Fix sending metrics
    
    * Format grafana json config
    
    * Update lib/jellyfish/room_service.ex
    
    Co-authored-by: Michał Śledź <michalsledz34@gmail.com>
    
    ---------
    
    Co-authored-by: Michał Śledź <michalsledz34@gmail.com>

--- lib/jellyfish_web/telemetry.ex
+++ lib/jellyfish_web/telemetry.ex
@@ -4,7 +4,11 @@ defmodule JellyfishWeb.Telemetry do
   use Supervisor
   import Telemetry.Metrics
   require Logger
-  alias JellyfishWeb.Telemetry.MetricsAggregator
+
+  @ice_received_event [Membrane.ICE, :ice, :payload, :received]
+  @ice_sent_event [Membrane.ICE, :ice, :payload, :sent]
+  @http_request_event [:jellyfish_web, :request]
+  @http_response_event [:jellyfish_web, :response]
 
   def start_link(arg) do
     Supervisor.start_link(__MODULE__, arg, name: __MODULE__)
@@ -25,8 +29,7 @@ defmodule JellyfishWeb.Telemetry do
       plug_cowboy_opts: [ip: metrics_ip]
     ]
 
-    children = [MetricsAggregator, {TelemetryMetricsPrometheus, metrics_opts}]
-
+    children = [{TelemetryMetricsPrometheus, metrics_opts}]
     Supervisor.init(children, strategy: :one_for_one)
   end
 
@@ -93,8 +96,66 @@ defmodule JellyfishWeb.Telemetry do
       metric_type.("vm.total_run_queue_lengths.total", []),
       metric_type.("vm.total_run_queue_lengths.cpu", []),
       metric_type.("vm.total_run_queue_lengths.io", [])
-    ]
-    # Jellyfish Metrics
-    |> Enum.concat(MetricsAggregator.metrics())
+    ] ++
+      [
+        # Jellyfish Metrics
+
+        # FIXME: At the moment, the traffic metrics track:
+        #   - Most HTTP traffic (Jellyfish API, HLS)
+        #   - ICE events (WebRTC)
+        #
+        # which means they don't count:
+        #   - WebSocket traffic
+        #   - RTP events (RTSP components don't use ICE)
+        #   - HTTP traffic related to metrics (not handled by Phoenix)
+        sum("jellyfish.traffic.ingress.webrtc.total.bytes",
+          event_name: @ice_received_event,
+          description: "Total WebRTC traffic received (bytes)"
+        ),
+        sum("jellyfish.traffic.egress.webrtc.total.bytes",
+          event_name: @ice_sent_event,
+          description: "Total WebRTC traffic sent (bytes)"
+        ),
+        sum("jellyfish.traffic.ingress.http.total.bytes",
+          event_name: @http_request_event,
+          description: "Total HTTP traffic received (bytes)"
+        ),
+        sum("jellyfish.traffic.egress.http.total.bytes",
+          event_name: @http_response_event,
+          description: "Total HTTP traffic sent (bytes)"
+        ),
+        last_value("jellyfish.rooms",
+          description: "Amount of rooms currently present in Jellyfish"
+        ),
+
+        # FIXME: Prometheus warns about using labels to store dimensions with high cardinality,
+        # such as UUIDs. For more information refer here: https://prometheus.io/docs/practices/naming/#labels
+        last_value("jellyfish.room.peers",
+          tags: [:room_id],
+          description: "Amount of peers currently present in a given room"
+        ),
+        sum("jellyfish.room.peer_time.total.seconds",
+          event_name: [:jellyfish, :room],
+          measurement: :peer_time_total,
+          tags: [:room_id],
+          description: "Total peer time accumulated for a given room (seconds)"
+        )
+      ]
+  end
+
+  def default_webrtc_metrics() do
+    :telemetry.execute(
+      [Membrane.ICE, :ice, :payload, :sent],
+      %{
+        bytes: 0
+      }
+    )
+
+    :telemetry.execute(
+      [Membrane.ICE, :ice, :payload, :received],
+      %{
+        bytes: 0
+      }
+    )
   end
 end
