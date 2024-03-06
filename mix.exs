0eb6625bca0baa31915fd5699a5ee691e649c7a1
Przemysław Rożnawski <roznawski.przemyslaw@gmail.com>
2024-03-06 15:09:58 +0100

    Update OpenApi spec

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Jellyfish.MixProject do
   def project do
     [
       app: :jellyfish,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
