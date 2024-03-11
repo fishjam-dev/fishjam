409988fec1afdb3a7dca320d0c2be1434e39626f
Karol Konkol <konkolisz@gmail.com>
2024-03-11 12:44:49 +0100

    Fix recording tests

--- test/jellyfish_web/controllers/component/recording_component_test.exs
+++ test/jellyfish_web/controllers/component/recording_component_test.exs
@@ -2,6 +2,8 @@ defmodule JellyfishWeb.Component.RecordingComponentTest do
   use JellyfishWeb.ConnCase
   use JellyfishWeb.ComponentCase
 
+  import Mox
+
   @s3_credentials %{
     accessKeyId: "access_key_id",
     secretAccessKey: "secret_access_key",
@@ -12,7 +14,11 @@ defmodule JellyfishWeb.Component.RecordingComponentTest do
   @path_prefix "path_prefix"
 
   describe "create recording component" do
+    setup :set_mox_from_context
+
     test "renders component with required options", %{conn: conn, room_id: room_id} do
+      mock_http_request()
+
       conn =
         post(conn, ~p"/room/#{room_id}/component",
           type: "recording",
@@ -30,10 +36,13 @@ defmodule JellyfishWeb.Component.RecordingComponentTest do
       assert_component_created(conn, room_id, id, "recording")
     end
 
+    setup :set_mox_from_context
+
     test "renders component when credentials are in passed in config", %{
       conn: conn,
       room_id: room_id
     } do
+      mock_http_request()
       Application.put_env(:jellyfish, :s3_credentials, @s3_credentials)
 
       conn =
@@ -51,6 +60,8 @@ defmodule JellyfishWeb.Component.RecordingComponentTest do
              } = model_response(conn, :created, "ComponentDetailsResponse")
 
       assert_component_created(conn, room_id, id, "recording")
+
+      Application.put_env(:jellyfish, :s3_credentials, nil)
     end
 
     test "renders errors when required options are missing", %{conn: conn, room_id: room_id} do
@@ -63,5 +74,15 @@ defmodule JellyfishWeb.Component.RecordingComponentTest do
       assert model_response(conn, :bad_request, "Error")["errors"] ==
                "S3 credentials has to be passed either by request or at application startup as envs"
     end
+  end
+
+  defp mock_http_request() do
+    expect(ExAws.Request.HttpMock, :request, 4, fn _method,
+                                                   _url,
+                                                   _req_body,
+                                                   _headers,
+                                                   _http_opts ->
+      {:ok, %{status_code: 200, headers: %{}}}
+    end)
   end
 end
