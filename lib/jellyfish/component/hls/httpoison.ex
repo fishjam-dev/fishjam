7045ea4a2b2f324dfef27986b4178969948e083f
Karol Konkol <56369082+Karolk99@users.noreply.github.com>
2024-04-15 11:46:15 +0200

    Fix ex aws requests (#180)

--- lib/jellyfish/component/hls/httpoison.ex
+++ lib/jellyfish/component/hls/httpoison.ex
@@ -6,14 +6,26 @@ defmodule Jellyfish.Component.HLS.HTTPoison do
   @impl true
   def request(method, url, body \\ "", headers \\ [], http_opts \\ []) do
     case HTTPoison.request(method, url, body, headers, http_opts) do
-      {:ok, %HTTPoison.Response{status_code: status, headers: headers, body: body}} ->
-        {:ok, %{status_code: status, headers: headers, body: body}}
-
-      {:ok, %HTTPoison.Response{status_code: status, headers: headers}} ->
-        {:ok, %{status_code: status, headers: headers}}
+      {:ok, %HTTPoison.Response{} = response} ->
+        {:ok, adapt_response(response)}
 
       {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
+        {:error, %{reason: reason}}
     end
+  end
+
+  defp adapt_response(response) do
+    # adapt the response to fit the shape expected by ExAWS
+    flat_headers =
+      Enum.flat_map(response.headers, fn
+        {name, vals} when is_list(vals) -> Enum.map(vals, &{name, &1})
+        {name, val} -> [{name, val}]
+      end)
+
+    %{
+      body: response.body,
+      status_code: response.status_code,
+      headers: flat_headers
+    }
   end
 end
