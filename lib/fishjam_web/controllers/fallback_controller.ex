3d336b7379b6f2929acd9cbd2fc012b07d8bff82
Radosław Szuma <radoslawszuma@gmail.com>
2024-05-10 15:00:49 +0200

    Add generic error logger handler

--- lib/fishjam_web/controllers/fallback_controller.ex
+++ lib/fishjam_web/controllers/fallback_controller.ex
@@ -1,7 +1,12 @@
 defmodule FishjamWeb.FallbackController do
   use FishjamWeb, :controller
 
+  require Logger
+
   def call(conn, {:error, status, reason}) do
+    # TODO FIXME
+    Logger.warning("Generic error handler status: #{status}, reason: #{reason}")
+
     conn
     |> put_resp_content_type("application/json")
     |> put_status(status)
