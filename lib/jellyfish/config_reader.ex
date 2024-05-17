71246bf9e0034f3564f5dd4280cc162326b16f93
Przemysław Rożnawski <roznawski.przemyslaw@gmail.com>
2024-05-17 11:30:10 +0200

    Fix other variables

--- lib/jellyfish/config_reader.ex
+++ lib/jellyfish/config_reader.ex
@@ -246,8 +246,8 @@ defmodule Fishjam.ConfigReader do
     get_env("FJ_GIT_COMMIT", "dev")
   end
 
-  def read_server_api_token() do
-    get_env("FJ_SERVER_API_TOKEN", nil)
+  def read_string(string, default \\ nil) do
+    get_env(string, default)
   end
 
   defp do_read_nodes_list_config(node_name_value, cookie, mode) do
