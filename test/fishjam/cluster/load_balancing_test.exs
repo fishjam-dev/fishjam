e7cfb471daded5189dca5d8ddcb4cb35565d3b77
Radosław Szuma <radoslawszuma@gmail.com>
2024-06-11 14:32:44 +0200

    Add more tests for websocket connected to different node

--- test/fishjam/cluster/load_balancing_test.exs
+++ test/fishjam/cluster/load_balancing_test.exs
@@ -5,8 +5,9 @@ defmodule Fishjam.Cluster.LoadBalancingTest do
 
   use ExUnit.Case, async: false
 
-  alias Fishjam.PeerMessage.Authenticated
-  alias FishjamWeb.{PeerSocket, WS}
+  alias Fishjam.PeerMessage
+  alias Fishjam.PeerMessage.{Authenticated, MediaEvent}
+  alias FishjamWeb.WS
 
   @token Application.compile_env(:fishjam, :server_api_token)
   @headers [
@@ -61,21 +62,71 @@ defmodule Fishjam.Cluster.LoadBalancingTest do
     assert_rooms_number_on_fishjam(node2, 1)
   end
 
-  @tag timeout: @max_test_duration
-  test "connect peer to other node" do
-    [node1, _node2] = @nodes
+  describe "peer websocket load balancing" do
+    setup do
+      [node1, _node2] = @nodes
 
-    response_body = add_room(node1)
-    assert %{"id" => room_id} = response_body["data"]["room"]
-    fishjam_instance1 = get_fishjam_address(response_body)
+      response_body = add_room(node1)
+      assert %{"id" => room_id} = response_body["data"]["room"]
+      fishjam_instance = get_fishjam_address(response_body)
 
-    response_body = add_peer(fishjam_instance1, room_id)
-    token = response_body["data"]["token"]
-    other_node = @nodes |> Enum.find(&(&1 != fishjam_instance1))
-    create_and_authenticate(token, other_node)
+      response_body = add_peer(fishjam_instance, room_id)
+      token = response_body["data"]["token"]
+      peer_id = get_in(response_body, ["data", "peer", "id"])
+      other_node = @nodes |> Enum.find(&(&1 != fishjam_instance))
 
-    delete_room(fishjam_instance1, room_id)
-    assert_receive {:disconnected, {:remote, 1000, "Room stopped"}}, 2_000
+      on_exit(fn ->
+        HTTPoison.delete("http://#{fishjam_instance}/room/#{room_id}", @headers)
+      end)
+
+      {:ok,
+       %{
+         room_id: room_id,
+         token: token,
+         room_instance: fishjam_instance,
+         other_instance: other_node,
+         peer_id: peer_id
+       }}
+    end
+
+    @tag timeout: @max_test_duration
+    test "delete room on other node", state do
+      create_and_authenticate(state.token, state.other_instance)
+      delete_room(state.room_instance, state.room_id)
+      assert_receive {:disconnected, {:remote, 1000, "Room stopped"}}, 2_000
+    end
+
+    @tag timeout: @max_test_duration
+    test "delete peer on other node", state do
+      create_and_authenticate(state.token, state.other_instance)
+      delete_peer(state.room_instance, state.room_id, state.peer_id)
+      assert_receive {:disconnected, {:remote, 1000, "Peer removed"}}, 2_000
+    end
+
+    @tag timeout: @max_test_duration
+    test "send connect media event", state do
+      ws = create_and_authenticate(state.token, state.other_instance)
+
+      data = Jason.encode!(%{"type" => "connect", "data" => %{"metadata" => nil}})
+      msg = PeerMessage.encode(%PeerMessage{content: {:media_event, %MediaEvent{data: data}}})
+      :ok = WS.send_binary_frame(ws, msg)
+
+      assert_receive %MediaEvent{data: data}, 1000
+      assert %{"type" => "connected"} = Jason.decode!(data)
+    end
+
+    @tag timeout: @max_test_duration
+    test "peer socket killed", state do
+      ws = create_and_authenticate(state.token, state.other_instance)
+
+      Process.unlink(ws)
+      Process.monitor(ws)
+      Process.exit(ws, :disconnected)
+      assert_receive {:DOWN, _ref, :process, ^ws, :disconnected}
+
+      room_state = get_room_state(state.room_instance, state.room_id)
+      assert Enum.all?(room_state["data"]["peers"], &(&1["status"] == "disconnected"))
+    end
   end
 
   defp add_room(fishjam_instance) do
@@ -110,6 +161,26 @@ defmodule Fishjam.Cluster.LoadBalancingTest do
              HTTPoison.delete("http://#{fishjam_instance}/room/#{room_id}", @headers)
 
     body
+  end
+
+  defp delete_peer(fishjam_instance, room_id, peer_id) do
+    assert {:ok, %HTTPoison.Response{status_code: 204, body: body}} =
+             HTTPoison.delete(
+               "http://#{fishjam_instance}/room/#{room_id}/peer/#{peer_id}",
+               @headers
+             )
+
+    body
+  end
+
+  defp get_room_state(fishjam_instance, room_id) do
+    assert {:ok, %HTTPoison.Response{status_code: 200, body: body}} =
+             HTTPoison.get(
+               "http://#{fishjam_instance}/room/#{room_id}",
+               @headers
+             )
+
+    Jason.decode!(body)
   end
 
   defp map_fishjam_address(fishjam) do
